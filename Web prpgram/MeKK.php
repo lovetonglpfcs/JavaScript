3467eac05b7462ff3ab166b654367bd807e46bdb
lovetonglpfcs <onew.suksamai@gmail.com>
2021-01-21 12:11:52 +0700

    update

---
+++ Web prpgram/MeKK.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+<center> 
+<?php 
+    $IDstudy = "6206021612024" ;
+    $name ="นายกิตติศักดิ์ สุขสมัย";
+    $address = "12 ม.5 ต.บางกระทึก อ.สามพราน จ.นครปฐม";
+    $tel = "0924850526";
+    echo "ID $IDstudy<br/>";
+    echo "Name $name<br/>";
+    echo "Address $address<br/>";
+    echo "tel $tel<br/>";
+?>
+</center> 
+</body>
+</html>
